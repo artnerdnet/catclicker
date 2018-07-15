3b0362edb42131cfbf7ef4267aa62c7459c036a8
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-15 12:29:28 -0300

    Update catModel.js
    Removed line breaks

--- js/models/catModel.js
+++ js/models/catModel.js
@@ -5,19 +5,16 @@ const model = {
             imgSrc: 'img/orson.png',
             clickCount: 0, 
         },
-        
         {
             name: 'Quentin',
             imgSrc: 'img/quentin.png',
             clickCount: 0, 
-        },
-        
+        },        
         {
             name: 'Jean',
             imgSrc: 'img/jean.png',
             clickCount: 0, 
         },
-
         {
             name: 'Alfred',
             imgSrc: 'img/alfred.png',
