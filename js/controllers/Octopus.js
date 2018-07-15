a41fbf779672e03a7ed7fea761d00735ff9201d0
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-15 12:31:15 -0300

    Update Octopus.js
    Removed line breaks

--- js/controllers/Octopus.js
+++ js/controllers/Octopus.js
@@ -4,20 +4,16 @@ const Octopus = {
         View.init();
         listView.init();
     },
-
     getAllCats: function(){
         return model.cats;
     },
-
     getCat: function() {
         return model.currentCat;
     },
-
     addToCounter: function() {
         model.currentCat.clickCount++;
         View.render();
     },
-
     setCat: function(cat) {
         model.currentCat = cat;
     }   
